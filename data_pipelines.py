9ed73c861140b772c53053e661cb7ce81dc086e9
Maria Boryslawska <70576728+boryslawskamaria@users.noreply.github.com>
2022-04-21 15:53:19 +0200

    Create data_pipelines.py

---
+++ data_pipelines.py
@@ -0,0 +1,25 @@
+# Import the data to a DataFrame
+departures_df = spark.read.csv('2015-departures.csv.gz', header=True)
+
+# Remove any duration of 0
+departures_df = departures_df.filter(departures_df[3] > 0)
+
+# # Add an ID column
+departures_df = departures_df.withColumn('id', F.monotonically_increasing_id())
+
+# # Write the file out to JSON format
+departures_df.write.json('output.json', mode='overwrite')
+
+# Import the file to a DataFrame and perform a row count
+annotations_df = spark.read.csv('annotations.csv.gz', sep='|')
+full_count = annotations_df.count()
+
+# Count the number of rows beginning with '#'
+comment_count = annotations_df.where(col('_c0').startswith('#')).count()
+
+# Import the file to a new DataFrame, without commented rows
+no_comments_df = spark.read.csv('annotations.csv.gz', sep='|', comment='#')
+
+# Count the new DataFrame and verify the difference is as expected
+no_comments_count = no_comments_df.count()
+print("Full count: %d\nComment count: %d\nRemaining count: %d" % (full_count, comment_count, no_comments_count))
