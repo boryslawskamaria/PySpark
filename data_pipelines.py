6fa41360cb9f224c728d4275c41131fabce2dd2d
Maria Boryslawska <70576728+boryslawskamaria@users.noreply.github.com>
2022-04-22 11:47:42 +0200

    Update data_pipelines.py

--- data_pipelines.py
+++ data_pipelines.py
@@ -23,3 +23,32 @@ no_comments_df = spark.read.csv('annotations.csv.gz', sep='|', comment='#')
 # Count the new DataFrame and verify the difference is as expected
 no_comments_count = no_comments_df.count()
 print("Full count: %d\nComment count: %d\nRemaining count: %d" % (full_count, comment_count, no_comments_count))
+
+# Split _c0 on the tab character and store the list in a variable
+tmp_fields = F.split(annotations_df['_c0'], '\t')
+
+# Create the colcount column on the DataFrame
+annotations_df = annotations_df.withColumn('colcount', F.size(tmp_fields))
+
+# Remove any rows containing fewer than 5 fields
+annotations_df_filtered = annotations_df.filter(~ (annotations_df["colcount"] < 5))
+
+# Count the number of rows
+final_count = annotations_df_filtered.count()
+print("Initial count: %d\nFinal count: %d" % (initial_count, final_count))
+
+
+# Split the content of _c0 on the tab character (aka, '\t')
+split_cols = F.split(annotations_df["_c0"], '\t')
+
+# Add the columns folder, filename, width, and height
+split_df = annotations_df.withColumn('folder', split_cols.getItem(0))
+split_df = split_df.withColumn('filename', split_cols.getItem(1))
+split_df = split_df.withColumn('width', split_cols.getItem(2))
+split_df = split_df.withColumn('height', split_cols.getItem(3))
+
+# Add split_cols as a column
+split_df = split_df.withColumn('split_cols', split_cols)
+
+
+
